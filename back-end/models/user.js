dfdceb3d3e9aa10643f616770589843892fe5d08
NathanRomanos <nathanromanos@hotmail.com>
2020-04-03 16:59:23 +1300

    major updates to the backend files

--- back-end/models/user.js
+++ back-end/models/user.js
@@ -1,1 +1,16 @@
+//Nathan's code:
 const mongoose = require('mongoose'); // since we are using mongoose we have to require it
+
+const userSchema = new mongoose.Schema({
+  _id : mongoose.Schema.Types.ObjectId,
+email : String,
+username : String,
+password : String,
+firstName : String,
+lastName : String,
+userDesc : String,
+profileImg : String
+});
+
+module.exports = mongoose.model('User', userSchema);
+//Nathan's code ends
